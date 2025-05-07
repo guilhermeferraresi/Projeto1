ee53b75489c1ae348973b782d9b4369e1cfecc39
Guilherme <gui.ferraresi2008@gmail.com>
2025-05-07 08:07:35 -0300

    Criando a controller, o Usuario e a View

---
+++ Projeto1/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Contato()
+        {
+            return View();
+        }
+    }
+}
